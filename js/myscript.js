d67e23e445474b0bacffdb81ec06d0e68d4b9b28
nchikore <nickchikore@gmail.com>
2016-05-05 02:30:01 +0100

    Added htaccess, added studio page, terms and conditions and created smart links

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,11 @@
-$(function() {
-
+$(document).ready(function(){
   "use strict";
+  initProcs();
+  setupTemplates();
+  parallax();
+});
+
+function initProcs() {
 
   var topoffset = 50; //variable for menu height
   var slideqty = $('#featured .item').length;
@@ -10,7 +15,7 @@ $(function() {
   $('#featured .item').eq(randSlide).addClass('active');
 
 
-  $('.fullheight').css('height', wheight); //set to window tallness  
+  $('.fullheight').css('height', wheight); //set to window tallness
 
 
   //replace IMG inside carousels with a background image
@@ -23,7 +28,7 @@ $(function() {
   //adjust height of .fullheight elements on window resize
   $(window).resize(function() {
     wheight = $(window).height(); //get the height of the window
-    $('.fullheight').css('height', wheight); //set to window tallness  
+    $('.fullheight').css('height', wheight); //set to window tallness
   });
 
 
@@ -56,8 +61,8 @@ $(function() {
 
   //Use smooth scrolling when clicking on navigation
   $('.navbar a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') === 
-      this.pathname.replace(/^\//,'') && 
+    if (location.pathname.replace(/^\//,'') ===
+      this.pathname.replace(/^\//,'') &&
       location.hostname === this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -83,5 +88,57 @@ $(function() {
   $('.carousel').carousel({
     pause: false
   });
+}
 
+function setupTemplates(){
+      var tariff = $('#packages').html(),
+      pkgs = Mustache.render(tariff, studio);
+      $('#studioPackages').append(pkgs);
+
+      var engineer = $('#engineers').html(),
+      ctlRoom = Mustache.render(engineer, studio);
+      $('#engineersPackages').append(ctlRoom);
+
+      var gallery = $('#gallery').html(),
+      pics = Mustache.render(gallery, studio);
+      $('#galleryPics').append(pics);
+
+      var navigation = $('#sitenav').html(),
+      nav = Mustache.render(navigation, studio);
+      $('#collapse').append(nav);
+
+      var terms = $('#tc').html(),
+      tcs = Mustache.render(terms, studio);
+      $('#studioTerms').append(tcs);
+}
+//parallax
+$(document).ready(function() {
+  if ($("#js-parallax-window").length) {
+    parallax();
+  }
 });
+
+$(window).scroll(function(e) {
+  if ($("#js-parallax-window").length) {
+    parallax();
+  }
+});
+
+function parallax(){
+  if( $("#js-parallax-window").length > 0 ) {
+    var plxBackground = $("#js-parallax-background");
+    var plxWindow = $("#js-parallax-window");
+
+    var plxWindowTopToPageTop = $(plxWindow).offset().top;
+    var windowTopToPageTop = $(window).scrollTop();
+    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
+
+    var plxBackgroundTopToPageTop = $(plxBackground).offset().top;
+    var windowInnerHeight = window.innerHeight;
+    var plxBackgroundTopToWindowTop = plxBackgroundTopToPageTop - windowTopToPageTop;
+    var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
+    var plxSpeed = 0.35;
+
+    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
+  }
+}
